aebde169975108f245d8c8aea57391ab33b1ba62
Kogay Irina <kogayie1995@gmail.com>
2020-10-18 19:47:19 +0300

    refactor js, consolidate hiding logic between the form and sections

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-const requestForm = document.getElementById('request-form');
-const requestButton = document.getElementById('request-button');
-
-requestButton.addEventListener('click', function() {
-    if (requestForm.style.height !== '900px') {
-        requestButton.innerHTML = 'Свернуть';
-        requestForm.style.height = '900px';
-        requestForm.style.opacity = '1';
-    } else {
-        requestForm.style.height = '0';
-        requestForm.style.opacity = '0';
-        requestButton.innerHTML = 'Записаться на консультацию';
-    }
-});
-
-function hideText(titleLink, containerDiv) {
+function hideContainer(titleLink, containerDiv, containerHeight, onShow, onHide) {
     titleLink.addEventListener('click', function(event) {
         event.preventDefault();
 
         if (containerDiv.style.height) {
             containerDiv.style.height = '';
+            containerDiv.style.opacity = '';
+            if (onHide) {
+                onHide();
+            }
         } else {
-            containerDiv.style.height = `${containerDiv.scrollHeight}px`;
+            containerDiv.style.opacity = '1';
+            containerDiv.style.height = `${containerHeight || containerDiv.scrollHeight}px`;
+            if (onShow) {
+                onShow();
+            }
         }
     });
 }
+
+const requestForm = document.getElementById('request-form');
+const requestButton = document.getElementById('request-button');
+
+hideContainer(
+    requestButton,
+    requestForm,
+    920,
+    () => requestButton.innerHTML = 'Свернуть',
+    () => requestButton.innerHTML = 'Записаться на консультацию'
+);
 
 const sections = document.getElementsByClassName('section');
 for (const section of sections) {
     const sectionTitle = section.getElementsByClassName('sectionTitle')[0];
     const sectionBody = section.getElementsByClassName('sectionBody')[0];
-    hideText(sectionTitle, sectionBody);
+    hideContainer(sectionTitle, sectionBody);
 }
